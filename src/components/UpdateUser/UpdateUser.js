c52171294bb862d51ed7cccdab87a159dcaf9898
Farhadcode <farhad.ahmed.aust@gmail.com>
2021-10-24 10:33:45 +0600

    cliend site added

--- src/components/UpdateUser/UpdateUser.js
+++ src/components/UpdateUser/UpdateUser.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import userEvent from '@testing-library/user-event';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router';
+import { useState } from 'react/cjs/react.development';
 
 const UpdateUser = () => {
+    const [singleUser, setSingleUser] = useState({});
+    const { id } = useParams();
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/users/${id}`)
+            .then(res => res.json())
+            .then(data => setSingleUser(data))
+    }, [])
     return (
         <div>
-            <h2>This is Update User</h2>
+            <h2>Update {singleUser.name}</h2>
+            <p><small>{id}</small></p>
         </div>
     );
 };
